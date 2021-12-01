b3c3da23553d6b9b25c972b246e03441d2072c6c
user <user@DESKTOP-7JBM641>
2021-12-01 23:16:17 +0800

    Добавьте файлы проекта.

---
+++ Laboratornaya 5/Form1.cs
@@ -0,0 +1,52 @@
+﻿using Laboratornaya_5.Object;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+
+namespace Laboratornaya_5
+{
+    public partial class Form1 : Form
+    {
+        MyRectangle myRect;
+        List<BaseObject> objects = new (); //добавляем список
+        Player player;
+        public Form1()
+        {
+            InitializeComponent();
+
+            player = new Player(pbMain.Width / pbMain.Height / 2, 0);
+            objects.Add(player);
+            objects.Add(new MyRectangle(50, 50, 0));
+            objects.Add(new MyRectangle(100, 100, 45));
+           
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics; // вытащили объект графики из события
+
+            g.Clear(Color.White);
+
+            foreach(var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+           
+        }
+    }
+}
---
+++ Laboratornaya 5/Object/MyRectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Laboratornaya_5.Object
+{
+    class MyRectangle : BaseObject
+    {
+        public MyRectangle(float x, float y, float angle) : base(x, y, angle)
+        {
+
+        }
+
+        // переопределяем Render
+        public override void Render(Graphics g)
+        {
+            // и запихиваем туда код из формы
+            g.FillRectangle(new SolidBrush(Color.Yellow), -25, -15, 50, 40);
+            g.DrawRectangle(new Pen(Color.Orange, 7), -25, -15, 50, 40);
+        }
+    }
+}
