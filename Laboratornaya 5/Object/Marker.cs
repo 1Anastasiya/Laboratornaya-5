d1cacd00933434b39346665b3f18c88884324609
1Анастасия <nastyaaaaaa19@gmail.com>
2021-12-02 12:45:37 +0800

    устранения ошибок в классе Marker

--- Laboratornaya 5/Object/Marker.cs
+++ Laboratornaya 5/Object/Marker.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
-using System.Drawing.Drawing2D;
+using System.Text;
 
 namespace Laboratornaya_5.Object
 {
@@ -17,8 +14,8 @@ namespace Laboratornaya_5.Object
         public override void Render(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Red), -3, -3, 6, 6);
-            g.FillEllipse(new Pen(Color.Red, 2), -6, -6, 12, 12);
-            g.FillEllipse(new Pen(Color.Red, 2), -10, -10, 20, 20);
+            g.DrawEllipse(new Pen(Color.Red, 2), -6, -6, 12, 12);
+            g.DrawEllipse(new Pen(Color.Red, 2), -10, -10, 20, 20);
         }
     }
 }
